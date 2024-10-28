b89c4de73efb403f594122aa345b9acf9bc64c8f
LeanSerra <46695152+LeanSerra@users.noreply.github.com>
2024-10-28 16:54:49 -0300

    Fixed warning triggered by incorrect field data type

--- dbmodels.py
+++ dbmodels.py
@@ -1,5 +1,6 @@
 from datetime import datetime
 from sqlmodel import Field, SQLModel, Column, Integer, DECIMAL
+from decimal import Decimal
 
 
 class Product(SQLModel, table=True, arbitrary_types_allowed=True):
@@ -9,6 +10,6 @@ class Product(SQLModel, table=True, arbitrary_types_allowed=True):
     brand: str
     image_url: str
     description: str
-    price: int = Field(sa_column=Column(DECIMAL(100, 2)))
+    price: Decimal = Field(sa_column=Column(DECIMAL(100, 2)))
     supermarket: str
     last_updated: datetime
